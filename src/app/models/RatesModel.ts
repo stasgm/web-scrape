19fb166b15ae09e86c9bdfe75d40bbfc8fe97ec7
Stanislau <stasgm@gmail.com>
2018-12-20 17:58:10 +0300

    wip

--- src/app/models/RatesModel.ts
+++ src/app/models/RatesModel.ts
@@ -1,6 +1,17 @@
-export interface IRatesModel {
+export interface ICurrency {
   id: number;
   name: string;
+}
+
+export interface IRate {
   date: Date;
   rate: number;
+}
+
+export interface ICurrencyRate {
+  currency: ICurrency;
+  rates: IRate[];
+}
+export interface IRateModel {
+  list: ICurrencyRate[];
 }
